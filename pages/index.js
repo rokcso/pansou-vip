979cfdcf6baaf8e7efd69758d4eb91bf756a3f98
waringhu <waringhu@163.com>
2023-08-30 14:18:15 +0800

    next.js init

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <>
+      <h1>pansou.vip</h1>
+      <p>
+        一个网盘资源聚合搜索工具，记住域名：<b>盘搜 VIP</b>
+      </p>
+      <p>构建中...敬请期待！</p>
+      <p>
+        <b>我的其他作品：</b>
+      </p>
+      <ul>
+        <li>
+          <a href="https://www.urlxcode.com/">
+            URLxcode.com - 在线 URL 编码、解码、解析工具
+          </a>
+        </li>
+        <li>
+          <a href="https://rok.ink/">rok.ink - 临时短链接服务</a>
+        </li>
+      </ul>
+    </>
+  );
+}
