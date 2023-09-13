3fdcd4fb1bfc6d3d250b9494552455a753766630
rokcso <hi@rokcso.com>
2023-09-13 14:21:30 +0800

    site version 1.0

---
+++ components/sitesList.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import styles from "./sitesList.module.css";
+
+export default function SitesList({ sites }) {
+  const groupedSites = groupArray(sites, 5);
+  return (
+    <>
+      <div className={styles.container}>
+        {groupedSites.map((group, index) => (
+          <div key={index} className={styles.row}>
+            {group.map((site) => (
+              <div key={site.id} className={styles.siteCard}>
+                <Link href={`/${site.id}`} className={styles.cardLink}>
+                  <div>
+                    <h2>{site.name}</h2>
+                  </div>
+                  <div>
+                    <p>{site.briefIntro}</p>
+                  </div>
+                </Link>
+              </div>
+            ))}
+            {group.length < 5 &&
+              Array.from({ length: 5 - group.length }).map((_, i) => (
+                <div
+                  key={`placeholder-${i}`}
+                  className={styles.placeholder}
+                ></div>
+              ))}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+function groupArray(arr, size) {
+  const groupedArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    groupedArr.push(arr.slice(i, i + size));
+  }
+  return groupedArr;
+}
