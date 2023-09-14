7e79959bf22f8636ecca1d902c05dce0319c8653
waringhu <waringhu@163.com>
2023-09-14 12:56:15 +0800

    add tips

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6d0abbb4d3bb61d7"; }
+/******/ 		__webpack_require__.h = function() { return "370d35bf1c9251e3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/layout.js
+++ components/layout.js
@@ -14,12 +14,15 @@ export default function Layout({ children }) {
             🚀 如果你有好用的云盘搜索工具，欢迎
             <a href="https://jinshuju.net/f/s590FY">提交收录</a>
           </p>
+          <p>💯 亲测满分为 👍👍👍👍👍</p>
+          <p>💫 所有已收录的工具将每月维护更新信息</p>
         </header>
         <main>{children}</main>
         <footer className={styles.footer}>
           <span>盘搜 VIP 💖 云盘搜索工具精选导航&nbsp;</span>
           <span>
-            Copyright &copy; 2023 <Link href="https://rokcso.com">rokcso</Link>
+            Copyright &copy; 2023{" "}
+            <Link href="https://rokcso.com">🍩 rokcso</Link>
             &nbsp;
           </span>
           <span>All rights reserved.</span>
