60a8b5a4555ceaae638e669c8aa3a7c8a96e08c4
rokcso <hi@rokcso.com>
2024-04-07 15:30:43 +0800

    add faq

---
+++ pages/faq.js
@@ -0,0 +1,15 @@
+import CustomHead from "../components/customHead";
+import Layout from "../components/layout";
+
+export default function FAQ() {
+  return (
+    <>
+      <CustomHead>
+        <Layout>
+          <h2>精选工具</h2>
+          <p>💯 亲测满分为 👍👍👍👍👍</p>
+        </Layout>
+      </CustomHead>
+    </>
+  );
+}
