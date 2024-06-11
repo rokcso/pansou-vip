282df6e8c4df8444934368efd7d3b078e177f6b1
rokcso <hi@rokcso.com>
2024-06-11 22:54:36 +0800

    filter finish

--- app/components/hero/index.js
+++ app/components/hero/index.js
@@ -1,6 +1,6 @@
 import styles from "./index.module.css";
 
-export default function ({ lastUpdateDateStr }) {
+export default function ({ lastUpdateDateStr, siteCount }) {
   const nowDate = new Date();
   const lastUpdateDate = new Date(lastUpdateDateStr);
   const daysAgo = Math.floor(
@@ -8,7 +8,26 @@ export default function ({ lastUpdateDateStr }) {
   );
 
   function handleDonateClick() {
-    alert("将 pansou.vip 分享给你的朋友们就是对我最好的鼓励！❤️");
+    // 希望复制到剪贴板的文本
+    const textToCopy = "推荐你一个人工亲测精选的网盘搜索工具导航网站：piansou.vip，真的挺好用的！";
+  
+    // 检查 Clipboard API 可用性
+    if (navigator.clipboard) {
+      // 使用 Clipboard API 将文本写入剪贴板
+      navigator.clipboard.writeText(textToCopy)
+        .then(() => {
+          // 写入剪贴板成功后的回调
+          alert("推荐文本已复制到剪贴板，快去分享给你的朋友们吧！❤️");
+        })
+        .catch((err) => {
+          // 写入剪贴板失败后的回调
+          console.error("无法复制文本到剪贴板: ", err);
+          alert("将 pansou.vip 分享给你的朋友们就是对我最好的鼓励！❤️");
+        });
+    } else {
+      // 如果 Clipboard API 不可用，使用传统的提示方式
+      alert("将 pansou.vip 分享给你的朋友们就是对我最好的鼓励！❤️");
+    }
   }
 
   return (
@@ -16,7 +35,7 @@ export default function ({ lastUpdateDateStr }) {
       <h1>人工亲测精选的网盘搜索工具导航</h1>
       <p>⭐ 坚持人工亲测精选，给你 VIP 一样的体验</p>
       <p>
-        💫 定期维护更新，最近更新时间：{lastUpdateDateStr}（{daysAgo} 天前）
+        💫 定期维护更新（已收录 {siteCount} 个实用网盘搜索工具），最近更新时间：{lastUpdateDateStr}（{daysAgo} 天前）
       </p>
       <p>
         👋 如果对你有所帮助，请收藏盘搜VIP，域名：pansou.vip，速记：
--- app/page.js
+++ app/page.js
@@ -8,14 +8,38 @@ import styles from "./page.module.css";
 import featuredSites from "../public/data/featuredSites.json";
 import userRecommendedSites from "../public/data/userRecommendedSites.json";
 import noTestSites from "../public/data/noTestSites.json";
+import forumSites from "../public/data/forum.json";
 
 export default function Home() {
   // 全局常量，数据最新更新时间（字符串格式）
   const lastUpdateDateStr = "2024-06-11";
 
-  // 平台筛选，用户选择的平台
+  // 统计已收录的网站的数量
+  const featuredSiteCount = featuredSites.length;
+  const userRecommendedSiteCount = userRecommendedSites.length;
+  const noTestSiteCount = noTestSites.length;
+  const forumSiteCount = forumSites.length;
+  const allSiteCount = featuredSiteCount + userRecommendedSiteCount + noTestSiteCount + forumSiteCount;
+
+
+  // 平台筛选，用户选择的平台的值，默认 all
   const [selectedPlatform, setSelectedPlatform] = useState("all");
-  const platforms = Array.from(new Set(featuredSites.flatMap((site) => site.platforms)));
+  const [userRecommendedSelectedPlatform, setUserRecommendedSelectedPlatform] =
+    useState("all");
+  const [noTestSelectedPlatform, setNoTestSelectedPlatform] = useState("all");
+
+  // 用户可用来筛选平台的选项，从 Site 数据中获取 platforms 数据，将其声明为 Set 对象可以直接去重，再转换为数组
+  const platformOptions = Array.from(
+    new Set(featuredSites.flatMap((site) => site.platforms))
+  );
+  const userRecommendedSitePlatformOptions = Array.from(
+    new Set(userRecommendedSites.flatMap((site) => site.platforms))
+  );
+  const noTestSitePlatformOptions = Array.from(
+    new Set(noTestSites.flatMap((site) => site.platforms))
+  );
+
+  // 平台原始值标识对应的中文值
   const platformTranslation = {
     ali: "阿里云盘",
     baidu: "百度网盘",
@@ -28,6 +52,8 @@ export default function Home() {
     weiyun: "腾讯微云",
     115: "115网盘",
   };
+
+  // 平台值的顺序
   const platformOrder = [
     "ali",
     "baidu",
@@ -40,43 +66,64 @@ export default function Home() {
     "weiyun",
     "115",
   ];
-  const sortedPlatforms = platformOrder
-    .filter((platform) => platforms.includes(platform));
+
+  // 排序之后的平台选项
+  const sortedPlatformOptions = platformOrder.filter((platform) =>
+    platformOptions.includes(platform)
+  );
+  const userRecommendedSortedPlatformOptions = platformOrder.filter(
+    (platform) => userRecommendedSitePlatformOptions.includes(platform)
+  );
+  const noTestSortedPlatformOptions = platformOrder.filter((platform) =>
+    noTestSitePlatformOptions.includes(platform)
+  );
+  
+  // 根据用户选择的平台过滤要显示的 Site 数据
   const filteredSites =
     selectedPlatform === "all"
       ? featuredSites
       : featuredSites.filter((site) =>
           site.platforms.includes(selectedPlatform)
         );
-  console.log(sortedPlatforms)
-  console.log(selectedPlatform)
+  const userRecommendedFilteredSites =
+    userRecommendedSelectedPlatform === "all"
+      ? userRecommendedSites
+      : userRecommendedSites.filter((site) =>
+          site.platforms.includes(userRecommendedSelectedPlatform)
+        );
+  const noTestFilteredSites =
+    noTestSelectedPlatform === "all"
+      ? noTestSites
+      : noTestSites.filter((site) =>
+          site.platforms.includes(noTestSelectedPlatform)
+        );
 
   return (
     <div>
-      <Hero lastUpdateDateStr={lastUpdateDateStr} />
+      <Hero lastUpdateDateStr={lastUpdateDateStr} siteCount={allSiteCount} />
       <AdBanner />
       <div className={styles.main}>
-        
         <div>
           <h2>精选工具</h2>
+          {/* 筛选组件 */}
           <div className={styles.filterDiv}>
-          <span>平台 👉 </span>
-          <select
-            value={selectedPlatform}
-            onChange={(e) => setSelectedPlatform(e.target.value)}
-          >
-            <option value={"all"}>全部</option>
-            {sortedPlatforms.map((platformKey) => {
-              const platformName = platformTranslation[platformKey] || platformKey;
-              return (
-                <option key={platformKey} value={platformKey}>
-                 {platformName} 
-                </option>
-              );
-            })}
-          </select>
-          <p>当前仅支持筛选「精选工具」，所有筛选稍后支持。</p>
-        </div>
+            <span>平台筛选 👉 </span>
+            <select
+              value={selectedPlatform}
+              onChange={(e) => setSelectedPlatform(e.target.value)}
+            >
+              <option value={"all"}>全部</option>
+              {sortedPlatformOptions.map((platformKey) => {
+                const platformName =
+                  platformTranslation[platformKey] || platformKey;
+                return (
+                  <option key={platformKey} value={platformKey}>
+                    {platformName}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
           <p>
             💯 以下工具均经过全面人工测试可用且好用，推荐使用（<b>非广告</b>
             ），推荐值 👍👍👍👍👍 为满分。
@@ -90,7 +137,27 @@ export default function Home() {
         </div>
         <div>
           <h2>用户推荐</h2>
-          
+          {/* 筛选组件 */}
+          <div className={styles.filterDiv}>
+            <span>平台筛选 👉 </span>
+            <select
+              value={userRecommendedSelectedPlatform}
+              onChange={(e) =>
+                setUserRecommendedSelectedPlatform(e.target.value)
+              }
+            >
+              <option value={"all"}>全部</option>
+              {userRecommendedSortedPlatformOptions.map((platformKey) => {
+                const platformName =
+                  platformTranslation[platformKey] || platformKey;
+                return (
+                  <option key={platformKey} value={platformKey}>
+                    {platformName}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
           <p>
             🥳
             以下工具均来自用户推荐，且经过一定程度的人工测试筛选，较为好用。如果你也有好用的网盘搜索工具，欢迎
@@ -107,12 +174,31 @@ export default function Home() {
           <br />
         </div>
         <div className={styles.siteList}>
-          {userRecommendedSites.map((cardInfo, index) => (
+          {userRecommendedFilteredSites.map((cardInfo, index) => (
             <Card key={index} cardInfo={cardInfo} />
           ))}
         </div>
         <div>
           <h2>备用工具</h2>
+          {/* 筛选组件 */}
+          <div className={styles.filterDiv}>
+            <span>平台筛选 👉 </span>
+            <select
+              value={noTestSelectedPlatform}
+              onChange={(e) => setNoTestSelectedPlatform(e.target.value)}
+            >
+              <option value={"all"}>全部</option>
+              {noTestSortedPlatformOptions.map((platformKey) => {
+                const platformName =
+                  platformTranslation[platformKey] || platformKey;
+                return (
+                  <option key={platformKey} value={platformKey}>
+                    {platformName}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
           <p>
             💢 以下工具均未经过全面人工测试，<b>仅保证能正常访问</b>
             ，在精选工具不满足需求时可自行选择以下工具作为备用。
@@ -120,7 +206,7 @@ export default function Home() {
           <br />
         </div>
         <div className={styles.siteList}>
-          {noTestSites.map((cardInfo, index) => (
+          {noTestFilteredSites.map((cardInfo, index) => (
             <Card key={index} cardInfo={cardInfo} />
           ))}
         </div>
