9073e8bcc1c9f31c8ddc6b4e264f409676dc0bc7
rokcso <hi@rokcso.com>
2024-06-20 10:57:25 +0800

    update hero copy link

--- app/components/hero/index.js
+++ app/components/hero/index.js
@@ -9,12 +9,14 @@ export default function ({ lastUpdateDateStr, siteCount }) {
 
   function handleDonateClick() {
     // 希望复制到剪贴板的文本
-    const textToCopy = "推荐你一个人工亲测精选的网盘搜索工具导航网站：piansou.vip，真的挺好用的！";
-  
+    const textToCopy =
+      "推荐你一个人工亲测精选的网盘搜索工具导航网站：https://www.pansou.vip/?via=hero，真的挺好用的！";
+
     // 检查 Clipboard API 可用性
     if (navigator.clipboard) {
       // 使用 Clipboard API 将文本写入剪贴板
-      navigator.clipboard.writeText(textToCopy)
+      navigator.clipboard
+        .writeText(textToCopy)
         .then(() => {
           // 写入剪贴板成功后的回调
           alert("推荐文本已复制到剪贴板，快去分享给你的朋友们吧！❤️");
@@ -35,7 +37,8 @@ export default function ({ lastUpdateDateStr, siteCount }) {
       <h1>人工亲测精选的网盘搜索工具导航</h1>
       <p>⭐ 坚持人工亲测精选，给你 VIP 一样的体验</p>
       <p>
-        💫 定期维护更新（已收录 {siteCount} 个实用网盘搜索工具），最近更新时间：{lastUpdateDateStr}（{daysAgo} 天前）
+        💫 定期维护更新（已收录 {siteCount} 个实用网盘搜索工具），最近更新时间：
+        {lastUpdateDateStr}（{daysAgo} 天前）
       </p>
       <p>
         👋 如果对你有所帮助，请收藏盘搜VIP，域名：pansou.vip，速记：
--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ import forumSites from "../public/data/forumSites.json";
 
 export default function Home() {
   // 全局常量，数据最新更新时间（字符串格式）
-  const lastUpdateDateStr = "2024-06-19";
+  const lastUpdateDateStr = "2024-06-20";
 
   // 统计已收录的网站的数量
   const featuredSiteCount = featuredSites.length;
