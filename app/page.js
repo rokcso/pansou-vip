ea5e8da9f6a80f14c082b710c0273c66d19b968a
rokcso <hi@rokcso.com>
2024-09-22 14:18:25 +0800

    add sites

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ import forumSites from "../public/data/forumSites.json";
 
 export default function Home() {
   // 全局常量，数据最新更新时间（字符串格式）
-  const lastUpdateDateStr = "2024-08-19";
+  const lastUpdateDateStr = "2024-09-22";
 
   // 统计已收录的网站的数量
   const featuredSiteCount = featuredSites.length;
