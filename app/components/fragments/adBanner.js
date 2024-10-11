254d02c92a6512d64628d1ee9b1a80e5738179c1
rokcso <hi@rokcso.com>
2024-10-11 10:24:00 +0800

    add random ad

--- app/components/fragments/adBanner.js
+++ app/components/fragments/adBanner.js
@@ -1,52 +1,28 @@
 import styles from "./adBanner.module.css";
 
 export default function AdBanner() {
+  const adInfo = [
+    {
+      title: "氢刻舟小铺",
+      description: "➡️ 超大流量卡，超低价，去看看",
+      url: "https://simhaoka.com/phone/index?id=8AECBC4E09973D2C2C8C637A99B4AA58",
+    },
+    {
+      title: "Skillix 小报童精选专栏",
+      description: "全新发布：小报童精选专栏特别优惠，最高 5 折！",
+      url: "https://skillix.cc/?utm_source=pansou.vip",
+    },
+  ];
+  const nowAd = adInfo[Math.floor(Math.random() * adInfo.length)];
   return (
-    // <a
-    //   href="https://rydnl.yhzu.cn/"
-    //   title="淘宝优惠商城"
-    //   className={styles.noteLink}
-    // >
-    //   <div className={styles.note}>
-    //     💰 <b>淘宝隐藏优惠券</b>：薅羊毛精选，每日严选大牌，1 折起购好物！
-    //   </div>
-    // </a>
-    // <a
-    //   href="/rubric"
-    //   title="盘搜VIP的网盘搜索工具评分标准"
-    //   className={styles.noteLink}
-    // >
-    //   <div className={styles.note}>
-    //     <b>《盘搜VIP 网盘搜索工具评分标准》上线啦！</b>🎉
-    //     平台收录的所有工具都将按照本评分标准进行人工测试，预计一周内完成。
-    //   </div>
-    // </a>
-    // <a
-    //   href="https://simhaoka.com/phone/index?id=8AECBC4E09973D2C2C8C637A99B4AA58"
-    //   title="氢刻舟小铺"
-    //   className={styles.noteLink}
-    //   rel="nofollow"
-    // >
-    //   <div className={styles.note}>
-    //     🎁 <b>超大流量卡，超低价，去看看 {">>>"}</b>
-    //   </div>
-    // </a>
-    // <a
-    //   href="https://www.xiaohongshu.com/user/profile/667a2a340000000007006fef?xhsshare=CopyLink&appuid=6046e26e00000000010085fe&apptime=1719310040"
-    //   title="Lunarye 美甲分享的小红书主页"
-    //   className={styles.noteLink}
-    // >
-    //   <div className={styles.note}>
-    //     <b>AD: </b>Lunarye™ Studio，手工穿戴甲寻宝 💝
-    //   </div>
-    // </a>
     <a
-      href="https://skillix.cc/?utm_source=pansou.vip"
-      title="Skillix 小报童精选专栏"
+      href={nowAd.url}
+      title={nowAd.title}
       className={styles.noteLink}
+      rel="nofollow"
     >
       <div className={styles.note}>
-        <b>全新发布：</b>小报童精选专栏特别优惠，最高 5 折！
+        <b>{nowAd.description}</b>
       </div>
     </a>
   );
